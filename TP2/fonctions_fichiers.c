636e28fce08faa0aedc6b79aec18cb2520b92529
Amaroke <hugo.amaroke@gmail.com>
2020-10-19 17:13:27 +0200

    19/10

---
+++ TP2/fonctions_fichiers.c
@@ -0,0 +1,21 @@
+#include "fonctions_fichiers.h"
+
+char** allouer_tab_2D(int n, int m){
+
+}
+
+void desallouer_tab_2D(char** tab, int n){
+
+}
+
+void afficher_tab_2D(char** tab, int n, int m){
+
+}
+
+void taille_fichier(const char* nomFichier, int* nbLig, int* nbCol){
+
+}
+
+char** lire_fichier(const char* nomFichier){
+
+}
---
+++ TP2/main.c
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "fonctions_fichiers.h"
+
+int main(void)
+{
+}
