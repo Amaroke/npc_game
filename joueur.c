0a4cc1f153913fb371c9f8df4dd3e228df9c4e58
mathieus1u <hugo.mathieu-steinbach5@etu.univ-lorraine.fr>
2020-11-14 13:55:44 +0100

    Rename

---
+++ joueur.c
@@ -0,0 +1,6 @@
+/**
+ *\file joueur.c
+ *\brief Gestion du joueur.
+ *\author MOSELLE Marie-Luc & MATHIEU STEINBACH Hugo
+ *\date 2 novembre 2020
+*/
---
+++ joueur.h
@@ -0,0 +1,18 @@
+/**
+ *\file joueur.h
+ *\brief Gestion du joueur.
+ *\author MOSELLE Marie-Luc & MATHIEU STEINBACH Hugo
+ *\date 2 novembre 2020
+*/
+
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include "sprite.h"
+
+typedef struct player_s
+{
+    sprite_t player;
+} player_t;
+
+#endif
