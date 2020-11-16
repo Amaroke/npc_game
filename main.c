1de1098664214e8570ea0a827517cf10d021bfdf
mathieus1u <hugo.mathieu-steinbach5@etu.univ-lorraine.fr>
2020-11-16 17:16:03 +0100

    test de sprite

--- main.c
+++ main.c
@@ -13,7 +13,5 @@
 */
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
     return EXIT_SUCCESS;
 }
--- tests/test_sprite.c
+++ tests/test_sprite.c
@@ -8,11 +8,16 @@
 #include "../sprite.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 void test_init_sprite()
 {
-    sprite_t sprite;
-    
+    sprite_t *sprite_test;
+    sprite_test = malloc(sizeof(sprite_t));
+    init_sprite(sprite_test, 0, 0, 0, 0, 0, false);
+    if (sprite_test->x != 0 && sprite_test->y != 0 && sprite_test->w != 0 && sprite_test->h != 0 && sprite_test->s != 0 && sprite_test->is_visible != false) {
+        printf("Il y a un problème, lors de l'initialisation d'un sprite avec comme paramètres : x=0, y=0, w=0, h=0, s=0, is_visible=false");
+    }
 }
 
 /**
@@ -20,8 +25,6 @@ void test_init_sprite()
 */
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
     test_init_sprite();
     return EXIT_SUCCESS;
 }
