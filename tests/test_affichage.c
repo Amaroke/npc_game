c37d1cabdc5193dbf4410aee452f1a585d03d3e1
MOBISARE <m.luc57@outlook.fr>
2020-11-16 17:59:51 +0100

    définition test_affichage.c

--- tests/test_affichage.c
+++ tests/test_affichage.c
@@ -6,15 +6,45 @@
 */
 
 #include "../affichage.h"
+#include "../fonctions_SDL.h"
+#include "../sprite.h"
+#include "../ressources.h"
+#include "../joueur.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 void test_apply_sprite()
 {
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    SDL_Texture *texture;
+    ressources_t ressources;
+    sprite_t *sprite;
+
+    sprite = malloc(sizeof(sprite_t));
+    init(&window, &renderer, &ressources);
+    texture = load_picture("ressources/sprites/sprite_test.bmp", renderer, 0, 0, 0);
+    init_sprite(sprite, 1, 1, 10, 10, 1, true);
+
+    apply_sprite(renderer, texture, sprite);
+    update_screen(renderer);
+
 }
 
 void test_apply_background()
 {
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    ressources_t ressources;
+    sprite_t *sprite;
+
+
+    sprite = malloc(sizeof(sprite_t));
+    init(&window, &renderer, &ressources);
+    init_sprite(sprite, 1, 1, 10, 10, 1, true);
+
+    apply_background(renderer, &ressources);
+    update_screen(renderer);
 }
 
 /**
@@ -22,9 +52,7 @@ void test_apply_background()
 */
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
+
     test_apply_sprite();
     test_apply_background();
     return EXIT_SUCCESS;
-}
