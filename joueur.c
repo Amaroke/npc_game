24ede9c1f348a9fa36c88201b470c7bf2133ed8f
MOBISARE <m.luc57@outlook.fr>
2020-11-23 16:37:45 +0100

    Création de la structure player et de ses fonctions

--- joueur.c
+++ joueur.c
@@ -7,4 +7,29 @@
 
 #include "joueur.h"
 
-player_t *player;
+void init_player(player_t *player, int x, int y, int s)
+{
+    player->x = x;
+    player->y = y;
+    player->s = s;
+}
+
+void move_left(player_t *player)
+{
+    player->x -= MOVING_STEP;
+}
+
+void move_right(player_t *player)
+{
+    player->x += MOVING_STEP;
+}
+
+void move_up(player_t *player)
+{
+    player->y += MOVING_STEP;
+}
+
+void move_down(player_t *player)
+{
+    player->y -= MOVING_STEP;
+}
--- joueur.h
+++ joueur.h
@@ -8,11 +8,47 @@
 #ifndef JOUEUR_H
 #define JOUEUR_H
 
+//Constantes
+
+#define MOVING_STEP 10
+
 #include "sprite.h"
 
 typedef struct player_s
 {
     sprite_t player;
+    int x;           /*!< Abscisses par rapport au centre de l'image.*/
+    int y;           /*!< Ordonnée par rapport au centre de l'image. */
+    int s;           /*!< Vitesse de déplacement du sprite. */
 } player_t;
+
+/**
+ *\brief La fonction initialise le player.
+ *\param player Le player à initialiser.
+ *\param x L'abscisse.
+ *\param y L'ordonnée.
+ *\param s La vitesse.
+*/
+void init_player(player_t *player, int x, int y, int s);
+
+/**
+ *\brief La fonction permet au joueur de se déplacer vers la gauche.
+*/
+void move_left(player_t *player);
+
+/**
+ *\brief La fonction permet au joueur de se déplacer vers la droite.
+*/
+void move_right(player_t *player);
+
+/**
+ *\brief La fonction permet au joueur de se déplacer vers le haut.
+*/
+void move_up(player_t *player);
+
+/**
+ *\brief La fonction permet au joueur de se déplacer vers le bas.
+*/
+void move_down(player_t *player);
 
 #endif
